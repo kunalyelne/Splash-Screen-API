12bb0cb59416d603300d7edb7de23e6be10db7ce
Kunal <kunal17058@gmail.com>
2022-01-29 15:58:01 +0530

    Add splash screen via API

--- app/src/main/java/com/example/splashscreenapi/MainActivity.kt
+++ app/src/main/java/com/example/splashscreenapi/MainActivity.kt
@@ -3,21 +3,34 @@ package com.example.splashscreenapi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.splashscreenapi.ui.theme.SplashScreenAPITheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {viewModel.isLoading.value}
+        }
         setContent {
             SplashScreenAPITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "Hello World!")
                 }
             }
         }
